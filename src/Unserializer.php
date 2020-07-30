33bc2aff3b6550cfaefba6a4bb47c2ca281d8dfc
zaioll <zaioll@protonmail.com>
2020-07-30 13:52:01 -0400

    Formatação arquivos PHP

--- src/Unserializer.php
+++ src/Unserializer.php
@@ -3,19 +3,20 @@
 namespace Zaioll\ActiveResource;
 
 use yii\base\Component;
+use Zaioll\ActiveResource\UnserializerInterface;
 
 /**
  * Class Unserializer
- * 
+ *
  * @package Zaioll\ActiveResource
  */
-abstract class Unserializer extends Component implements UnserializerInterface {
-
-	/**
-	 * @inheritdoc
-	 */
-	public function unserialize($data, $asArray = true) {
-
-		return $asArray ? (array) $data : $data;
-	}
+abstract class Unserializer extends Component implements UnserializerInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function unserialize($data, $asArray = true)
+    {
+        return $asArray ? (array) $data : $data;
+    }
 }
--- src/UnserializerInterface.php
+++ src/UnserializerInterface.php
@@ -4,16 +4,16 @@ namespace Zaioll\ActiveResource;
 
 /**
  * Interface UnserializerInterface
- * 
+ *
  * @package Zaioll\ActiveResource
  */
-interface UnserializerInterface {
-
-	/**
-	 * Unserialize data from JSON, XML, CSV, etc.
-	 * @param string $data
-	 * @param bool $asArray
-	 * @return mixed
-	 */
-	public function unserialize($data, $asArray = true);
+interface UnserializerInterface
+{
+    /**
+     * Unserialize data from JSON, XML, CSV, etc.
+     * @param string $data
+     * @param bool $asArray
+     * @return mixed
+     */
+    public function unserialize($data, $asArray = true);
 }
